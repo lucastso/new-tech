cf4ac208b4137cf0ee6c04a1723fdfcb51f1a2ef
lucastso <lucastassisouza@gmail.com>
2022-06-16 15:56:46 -0300

    feat: removed author from creating post

--- resources/views/posts/update.blade.php
+++ resources/views/posts/update.blade.php
@@ -5,7 +5,6 @@
         <input type="text" name="titulo" class="border border-black">
         <input type="text" name="imagem" class="border border-black">
         <input type="text" name="conteudo" class="border border-black">
-        <input type="text" name="autor" class="border border-black">
         <input type="text" name="categoria" class="border border-black">
         <button type="submit">Atualizar</button>
     </form>
